e0deb8cb91b1d1f65cfa087fd049ae3392e4c3d9
adogus <atiladogu@gmail.com>
2022-02-20 01:21:30 +0300

    added new scenes, coin panel prototype

---
+++ Assets/Scripts/CoinPanel.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinPanel : MonoBehaviour
+{
+    public Button button;
+    public Image indicatorImage;
+    public Text priceText;
+    public Text coinText;
+    public Image coinIcon;
+
+    public AnimationCurve curve;
+
+    
+    void Start()
+    {
+
+    }
+    
+    public void Init()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+}
