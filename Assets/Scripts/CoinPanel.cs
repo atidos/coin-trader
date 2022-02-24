f84bd1152ea95bf308e6e3129e7a0ba91770c5d5
eylul-commits <eylul.erdinc@bahcesehir.edu.tr>
2022-02-25 01:03:30 +0300

    fixed bar script, rearranged time thing in coin panel

--- Assets/Scripts/CoinPanel.cs
+++ Assets/Scripts/CoinPanel.cs
@@ -14,18 +14,21 @@ public class CoinPanel : MonoBehaviour
     float current = 0; //price in current frame
     float price = 0; //price after calculating with priceConstant
     int priceConstant = 1000; // value to multiply with current price
-    int timeConstant = 10; // value to divide current position on curve, helps determining the lifetime of the coin/curve
+    int timeConstant = 50; // value to divide current position on curve, helps determining the lifetime of the coin/curve
+
+    float initTime;
 
     void Start()
     {
-        indicatorImage.color = Color.green; 
+        indicatorImage.color = Color.green;
+        initTime = Time.time;
     }
     
-    public void Init( Text cText , Image cImage, AnimationCurve aCurve,int pCons, int tCons)
+    public void Init( string cText , Sprite cSprite, AnimationCurve aCurve,int pCons, int tCons)
     {
         //TODO: find a way to get partial inputs in Init function and take ungiven values as defaults above.
-        coinText = cText;
-        coinIcon = cImage;
+        coinText.text = cText;
+        coinIcon.sprite = cSprite;
         curve = aCurve;
         priceConstant = pCons;
         timeConstant = tCons;
@@ -33,7 +36,7 @@ public class CoinPanel : MonoBehaviour
     
     void Update()
     {
-        current = curve.Evaluate(Time.time/timeConstant); //calculates the current price using the curve and timeConstant
+        current = curve.Evaluate((Time.time-initTime)/timeConstant); //calculates the current price using the curve and timeConstant
         price = current * priceConstant; 
         priceText.text = Mathf.Floor(price).ToString(); //this and the above line shape the price tag
 
